ab2e97c84af5e31c3401a547ffbf81e146abaad6
W-Snack <90415228+W-Snack@users.noreply.github.com>
2024-12-26 22:54:37 +0300

    Update ops_seq.cpp

--- tasks/seq/fomin_v_sentence_count/src/ops_seq.cpp
+++ tasks/seq/fomin_v_sentence_count/src/ops_seq.cpp
@@ -21,9 +21,17 @@ bool fomin_v_sentence_count::SentenceCountSequential::validation() {
 bool fomin_v_sentence_count::SentenceCountSequential::run() {
   internal_order_test();
   // Подсчитываем количество предложений
+  bool in_sentence = false;
   for (int i = 0; input_[i] != '\0'; ++i) {
-    if (ispunct(input_[i]) && (input_[i] == '.' || input_[i] == '!' || input_[i] == '?')) {
-      sentence_count++;
+    if (input_[i] == '.' || input_[i] == '!' || input_[i] == '?') {
+      if (!in_sentence) {
+        sentence_count++;
+        in_sentence = true;
+      }
+    } else if (isspace(input_[i])) {
+      in_sentence = false;
+    } else {
+      in_sentence = true;
     }
   }
   return true;
