bfc4320d950cb40b92c9c50ee0572fcadc62fe72
W-Snack <90415228+W-Snack@users.noreply.github.com>
2024-12-27 20:51:16 +0300

    Update ops_mpi.cpp

--- tasks/mpi/fomin_v_sentence_count/src/ops_mpi.cpp
+++ tasks/mpi/fomin_v_sentence_count/src/ops_mpi.cpp
@@ -10,59 +10,55 @@ using namespace std::chrono_literals;
 bool fomin_v_sentence_count::SentenceCountParallel::pre_processing() {
   internal_order_test();
 
-    if (world.rank() == 0) {
-      if (taskData->inputs[0] == nullptr) {
-        return false;
-      }
-      input_ = reinterpret_cast<char*>(taskData->inputs[0]);
+  if (world.rank() == 0) {
+    if (taskData->inputs[0] == nullptr) {
+      return false;
     }
+    input_ = reinterpret_cast<char *>(taskData->inputs[0]);
+  }
 
-    int input_size;
-    if (world.rank() == 0) {
-      input_size = static_cast<int>(strlen(input_));
+  int input_size;
+  if (world.rank() == 0) {
+    input_size = static_cast<int>(strlen(input_));
+  }
+  broadcast(world, input_size, 0);
+
+  int chunk_size = input_size / world.size();
+  local_input.resize(chunk_size);
+
+  if (world.rank() == 0) {
+    for (int proc = 1; proc < world.size(); ++proc) {
+      int start = proc * chunk_size;
+      world.send(proc, input_ + start, chunk_size, proc, 0);
     }
-    broadcast(world, input_size, 0);
+  } else {
+    world.recv(world, local_input.data(), chunk_size, 0, 0);
+  }
 
-    int chunk_size = input_size / world.size();
-    local_input.resize(chunk_size);
-
-    if (world.rank() == 0) {
-      for (int proc = 1; proc < world.size(); ++proc) {
-        int start = proc * chunk_size;
-        send(world, input_ + start, chunk_size, proc, 0);
-      }
-      // Handle the case where input_size is not divisible by world.size()
-      if (input_size % world.size() != 0) {
-        // Additional handling if necessary
-      }
-    } else {
-      recv(world, local_input.data(), chunk_size, 0, 0);
-    }
-
-    local_sentence_count = 0;
-    return true;
+  local_sentence_count = 0;
+  return true;
 }
 
 bool fomin_v_sentence_count::SentenceCountParallel::validation() {
   internal_order_test();
-    if (world.rank() == 0) {
-      return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1;
-    }
-    return true;
+  if (world.rank() == 0) {
+    return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1;
+  }
+  return true;
 }
 
 bool fomin_v_sentence_count::SentenceCountParallel::run() {
   internal_order_test();
 
-    for (int i = 0; i < local_input.size(); ++i) {
-      if (local_input[i] == '.' || local_input[i] == '!' || local_input[i] == '?') {
-        local_sentence_count++;
-      }
+  for (size_t i = 0; i < local_input.size(); ++i) {
+    if (local_input[i] == '.' || local_input[i] == '!' || local_input[i] == '?') {
+      local_sentence_count++;
     }
+  }
 
-    reduce(world, local_sentence_count, sentence_count, boost::mpi::sum<int>(), 0);
+  reduce(world, local_sentence_count, sentence_count, std::plus, 0);
 
-    return true;
+  return true;
 }
 
 bool fomin_v_sentence_count::SentenceCountParallel::post_processing() {
@@ -86,13 +82,13 @@ bool fomin_v_sentence_count::SentenceCountParallel::post_processing() {
 bool fomin_v_sentence_count::SentenceCountSequential::pre_processing() {
   internal_order_test();
 
-    if (world.rank() == 0) {
-      if (taskData->outputs[0] == nullptr) {
-        return false;
-      }
-      reinterpret_cast<int*>(taskData->outputs[0])[0] = sentence_count;
+  if (world.rank() == 0) {
+    if (taskData->outputs[0] == nullptr) {
+      return false;
     }
-    return true;
+    reinterpret_cast<int *>(taskData->outputs[0])[0] = sentence_count;
+  }
+  return true;
 }
 
 bool fomin_v_sentence_count::SentenceCountSequential::validation() {
